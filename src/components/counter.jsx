a857bd3350f349a6d4332b359066e039d184a21d
khalid_munir <khalid_munir@icloud.com>
2018-09-01 21:22:47 +0100

    passing props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 class Counter extends Component { 
     state = {
-        count: 0,
+        count: this.props.value,
     }
 
     handleIncrement = product => {  
