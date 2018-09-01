055e1ee3a83d41b1c70a143cca8f4f706e1870bc
khalid_munir <khalid_munir@icloud.com>
2018-09-01 19:32:36 +0100

    example of not binding this

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,6 +5,17 @@ class Counter extends Component {
         listItems: [ /* 'Luke', 'Han', 'Leia', 'Chewy', 'Jabba'*/ ]
     }
 
+    handleIncrement() {
+
+        // this will error out as 'undefined' if you inseact the console - 
+        // This is because you dont have access to the 'this' variable as you might see it, 
+        // we should use ES6 style arrow functions, thus allowing us to bind this
+        // without the cumbersome registering it in the constructor for every class.
+        //
+        // this is good news for react 16 onwards - :)
+        console.log('Increment Clicked' , this)
+    }
+
     render() {
         
         let classes = this.GetBadgeClasses();
@@ -12,7 +23,11 @@ class Counter extends Component {
         return (
             <React.Fragment>
             <span className={ classes }> { this.formattedCount() } </span>
-            <button className='btn btn-secondary btn-sm'>Increment</button>
+            <button 
+                onClick={this.handleIncrement} 
+                className='btn btn-secondary btn-sm'>
+                Increment
+            </button>
             { this.state.listItems.length === 0 && 'Please add items to List'}
             {this.renderList()}
 
