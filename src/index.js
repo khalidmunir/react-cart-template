bf893aa9a4ba6a7af1eefe00177e58b79c0b2061
khalid_munir <khalid_munir@icloud.com>
2018-09-02 00:05:12 +0100

    forgot to save index.js as well in last commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Counters from './components/counters'
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
