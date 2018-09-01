8febf9f1fd4934a42c8a0566e2d607fb75274260
khalid_munir <khalid_munir@icloud.com>
2018-09-01 19:09:18 +0100

    refactoring code - clean up the render function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,16 +6,21 @@ class Counter extends Component {
 
     render() {
         
-        // moved common labels for better readability
-        let classes = 'badge m-2 badge-'
-        classes += (this.state.count === 0) ? 'warning' : 'primary'
-
+        let classes = this.GetBadgeClasses();
+        
         return (
-        <React.Fragment>
+            <React.Fragment>
             <span className={ classes }> { this.formattedCount() } </span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
         </React.Fragment>
         )
+    }
+    
+    GetBadgeClasses() {
+        // moved out of render method - don't bloat the render function - where possible  
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
     }
 
     formattedCount() {
