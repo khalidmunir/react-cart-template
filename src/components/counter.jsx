b677f94a5bc93bf8087b1d6270d066edc56e2c9b
khalid_munir <khalid_munir@icloud.com>
2018-09-01 18:59:37 +0100

    inline classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -12,7 +12,7 @@ class Counter extends Component {
     render() {
         return (
         <React.Fragment>
-            <span style={this.styles} className='badge badge-primary m-2'> { this.formattedCount() } </span>
+            <span style={ { fontSize: 30 } } className='badge badge-primary m-2'> { this.formattedCount() } </span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
         </React.Fragment>
         )
